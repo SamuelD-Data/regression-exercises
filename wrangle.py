a153dc60fd571ac93395d4d9b782dde2d117fa3b
Samuel Davila <samuelrdavila@gmail.com>
2020-09-29 15:46:14 -0500

    added wrangle grades function

--- wrangle.py
+++ wrangle.py
@@ -56,4 +56,10 @@ def minmax_scaler(train, validate, test):
     # returning scaled data sets
     return train, validate, test
 
+def wrangle_grades():
+    grades = pd.read_csv("student_grades.csv")
+    grades.drop(columns="student_id", inplace=True)
+    grades.replace(r"^\s*$", np.nan, regex=True, inplace=True)
+    df = grades.dropna().astype("int")
+    return df
 
